9dc80cf57ca60712152cd95988f11633ed41ca14
Antonio J <Antonio J@LAPTOP-PGI9D2JV>
2025-06-11 14:37:46 +0200

    Creada ventanaPrincipal
    Placeholder en comportamiento botones. Faltan iconos e imagenes

--- pds/src/main/java/vistas/VentanaPrincipal.java
+++ pds/src/main/java/vistas/VentanaPrincipal.java
@@ -1,12 +1,136 @@
 package vistas;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 import modelo.Usuario;
 import umu.pds.Controlador;
 
 public class VentanaPrincipal {
 
-	public VentanaPrincipal(Controlador controlador, Usuario usuario) {
-		// TODO Auto-generated constructor stub
-	}
+	private Usuario usuario;
+	private Controlador controlador;
 
+	private JFrame frame;
+
+	public VentanaPrincipal(Controlador controlador, Usuario usuario) {
+		this.controlador = controlador;
+		this.usuario = usuario;
+
+		// Crear el marco principal
+		frame = new JFrame("Copialingo - Principal");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // TODO: Editar en futuro
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setSize((int) (screenSize.width * 0.7), (int) (screenSize.height * 0.75));
+
+		// Establecemos el panel contenedor
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		frame.setContentPane(contentPane);
+
+		// Componentes de ventana
+		JLabel imagenUs = new JLabel("");
+
+		JLabel nombreUs = new JLabel(usuario.getNombreUs());
+		JLabel tiempoUs = new JLabel("Tiempo uso total: " + Integer.toString(usuario.getTiempoUso()));
+
+		JLabel imagenTckt = new JLabel("");
+		JLabel numeroTckt = new JLabel(Integer.toString(usuario.getTickets()));
+		JLabel rachaAct = new JLabel("Racha actual: " + Integer.toString(usuario.getRachaActual()));
+		JLabel rachaMax = new JLabel("Mejor racha: " + Integer.toString(usuario.getMejorRacha()));
+
+		JButton btnImportar = new JButton("Importar curso");
+		JButton btnAbrir = new JButton("Abrir curso");
+
+		// Panel para info de usuario a la izquierda
+		JPanel panelUs = new JPanel();
+		panelUs.setLayout(new BoxLayout(panelUs, BoxLayout.Y_AXIS));
+		panelUs.add(nombreUs);
+		panelUs.add(Box.createVerticalStrut(5));
+		panelUs.add(tiempoUs);
+
+		// Panel para info tickets
+		JPanel panelTckt = new JPanel();
+		panelTckt.setLayout(new BoxLayout(panelTckt, BoxLayout.X_AXIS));
+		panelTckt.add(numeroTckt);
+		panelTckt.add(Box.createHorizontalStrut(5));
+		panelTckt.add(imagenTckt);
+
+		// Panel para info rachas
+		JPanel panelRacha = new JPanel();
+		panelRacha.setLayout(new BoxLayout(panelRacha, BoxLayout.X_AXIS));
+		panelRacha.add(rachaAct);
+		panelRacha.add(Box.createHorizontalStrut(5));
+		panelRacha.add(rachaMax);
+
+		// Panel para organizar info + rachas
+		JPanel panelInfo = new JPanel();
+		panelInfo.setLayout(new BoxLayout(panelInfo, BoxLayout.Y_AXIS));
+		panelInfo.add(panelTckt);
+		panelInfo.add(Box.createVerticalStrut(5));
+		panelInfo.add(panelRacha);
+
+		// Creamos panel superior con info usuario
+		JPanel panelSup = new JPanel();
+		panelSup.setLayout(new BoxLayout(panelSup, BoxLayout.X_AXIS));
+		panelSup.add(imagenUs);
+		panelSup.add(Box.createHorizontalStrut(5));
+		panelSup.add(panelUs);
+		panelSup.add(Box.createHorizontalGlue());
+		panelSup.add(panelInfo);
+		contentPane.add(panelSup, BorderLayout.NORTH);
+
+		// Creamos panel que contendra los botones de la aplicacion
+		JPanel panelBtn = new JPanel();
+		panelBtn.setLayout(new BoxLayout(panelBtn, BoxLayout.X_AXIS));
+		panelBtn.add(Box.createHorizontalGlue());
+		panelBtn.add(btnImportar);
+		panelBtn.add(Box.createHorizontalStrut(10));
+		panelBtn.add(btnAbrir);
+		panelBtn.add(Box.createHorizontalGlue());
+
+		// Creamos panel que contendrá panel de botones (lo centra)
+		JPanel contenedorBtn = new JPanel();
+		contenedorBtn.setLayout(new BoxLayout(contenedorBtn, BoxLayout.Y_AXIS));
+		contenedorBtn.add(Box.createVerticalGlue());
+		contenedorBtn.add(panelBtn);
+		contenedorBtn.add(Box.createVerticalGlue());
+
+		contentPane.add(contenedorBtn, BorderLayout.CENTER);
+
+		// Comportamiento botones
+
+		// Evento para importar curso
+		btnImportar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Importar curso");
+			}
+		});
+		
+		// Evento para abrir curso
+		btnAbrir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Abrir curso");
+			}
+		});
+
+		// Mostrar la ventana
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+	}
 }
